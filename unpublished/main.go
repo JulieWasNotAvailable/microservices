868368752a21b152c9d7ed9d4a007d7d40cd48b6
JulieWasNotAvailable <i.gurtovenko.sas@gmail.com>
2025-04-27 00:17:56 +0300

    uncommented consumers

--- unpublished/main.go
+++ unpublished/main.go
@@ -52,9 +52,9 @@ func main() {
 		return c.JSON("Welcome to unpublished beats service")
 	})
 
-	// go consumer.StartConsumer("beat_mfcc2", mfcc_channel)
-	// go consumer.StartConsumer("delete_approve", delete_approve_channel)
-	// go consumer.StartConsumerFileUpdate("beat_files_updates", unpBeatService, metadataBeatService)
+	go consumer.StartConsumer("beat_mfcc2", mfcc_channel)
+	go consumer.StartConsumer("delete_approve", delete_approve_channel)
+	go consumer.StartConsumerFileUpdate("beat_files_updates", unpBeatService, metadataBeatService)
 
 	app.Listen(":7772")
 }
